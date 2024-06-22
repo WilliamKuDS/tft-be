da09c893522f0babef33796e6c269e9bfa938fb6
WilliamKuDS <williamku@pm.me>
2024-06-21 20:05:48 -0500

    Reverted previous commit, changed allowed_hosts to *

--- tft_django/settings.py
+++ tft_django/settings.py
@@ -29,11 +29,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-#
-# CORS_ALLOWED_ORIGINS = [
-#     "http://127.0.0.1:3000",
-# ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
